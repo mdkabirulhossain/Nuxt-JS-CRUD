b735dea9130a12ad836a53909e1dcb3c7d08c83e
Md. Kabirul Hossain <mdkabirulhossainj@gmail.com>
2024-10-27 02:34:54 +0600

    Student Created successfully

---
+++ components/loading.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="spinner-border text-info" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>{{ title }}...
+    </div>
+</template>
+
+<script>
+export default {
+    name: "loading",
+    props: {
+        title: String
+    }
+}
+</script>
---
+++ pages/about-us.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>About</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+useHead({
+    title: 'About us page',
+
+})
+</script>
